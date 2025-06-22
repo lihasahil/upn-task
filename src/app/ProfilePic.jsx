7f28726bab83a19a221bcf3afecdd0094c289286
lihasahil <sahilshrestha106@gmail.com>
2025-06-22 17:31:25 +0545

    feat(profile): add UI component for profile picture upload

--- src/app/ProfilePic.jsx
+++ src/app/ProfilePic.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProfilePic() {
   return (
     <div className="flex justify-center items-center">
-      <div className="bg-gradient-to-r from-yellow-400 to-white p-1 rounded-full">
+      <div className="bg-gradient-to-l from-yellow-400 to-white p-1 rounded-full">
         <div className="bg-white rounded-full p-1">
           <img
             src="https://th.bing.com/th/id/OIP.IGNf7GuQaCqz_RPq5wCkPgHaLH?rs=1&pid=ImgDetMain"
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,7 +4,6 @@ import ProfilePic from "./ProfilePic";
 const App = () => {
   return (
     <div className="text-red-500">
-      <ProfilePic />
     </div>
   );
 };
